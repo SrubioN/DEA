9799c73c80cfe53ca482b358b52593eab91ced40
aaron.ponceuv <aaron.ponce@alumnos.uv.cl>
2020-08-04 14:09:37 -0400

    Agrega lectura json

--- src/app/@core/components/map/map.component.ts
+++ src/app/@core/components/map/map.component.ts
@@ -1,15 +1,9 @@
+import { ReadExcelService } from './../../../read-excel.service';
 
 import { Component, OnInit } from '@angular/core';
 import { MapService } from '@core/services/map.service';
-<<<<<<< Updated upstream
-import { SendRequest }from '@core/services/sendrequest.service';
-
-//import { HttpClientModule } from '@angular/common/http';
-
-=======
-import { ConstantPool, compileNgModule, ThrowStmt } from '@angular/compiler';
-import { forkJoin } from "rxjs";
->>>>>>> Stashed changes
+import { ConstantPool } from '@angular/compiler';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-map',
@@ -17,52 +11,25 @@ import { forkJoin } from "rxjs";
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-<<<<<<< Updated upstream
-  public dir:any;
-  constructor(private map: MapService,private rq: SendRequest) { }
-  
-  ngOnInit() {
-    
-    this.map.initializeMap();
-=======
 
   archivo : any
 
   constructor(private map: MapService, private readexcelservice: ReadExcelService) { }
 
-    ngOnInit() {
+  ngOnInit() {
     this.map.buildMap();
->>>>>>> Stashed changes
     this.map.setLocationButton();
     this.map.setLocations();
     this.map.setNavigationControl();
+    //console.log(this.readexcelservice.get());
+    //console.log(this.readexcelservice.getjson())
+
+    this.readexcelservice.getJSON().subscribe(data => {
+      console.log(data);
+    });
     
-    this.rq.send().subscribe(directions => {
-      this.dir=directions;
-      console.log(directions);
-    } );
-
-<<<<<<< Updated upstream
     
-
-    //https://api.mapbox.com/directions-matrix/v1/mapbox/driving/A;B;C;D;E?sources=0&destinations=1;2;3;4&access_token=.
-=======
-    //console.log(this.archivo)
-
-
+        
   }
-  async this.readexcelservice.get_excel();{
-    await  this.readexcelservice.set_excel();
-    //this.setArchivo(this.readexcelservice.get_excel());
-  //  console.log(this.archivo);
-  }
-
-
-  setArchivo(archivo);{
-    this.archivo = archivo;
->>>>>>> Stashed changes
-  }
-    
-
 
 }
--- src/app/read-excel.service.ts
+++ src/app/read-excel.service.ts
@@ -1,67 +1,45 @@
 import { Injectable } from '@angular/core';
 import * as XLSX from 'xlsx';
 import { HttpClient } from '@angular/common/http';
-
-import { of } from 'rxjs';
-import { tap } from 'rxjs/operators';
-
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ReadExcelService {
 
-  public excel: any
+  public data: any
 
   constructor(private http: HttpClient) { }
 
-
-  async get_excel(){
-    let urls = "/assets/excel.xlsx";
+  get(){
+    let url = "/assets/excel.xlsx";
     let req = new XMLHttpRequest();
-    req.open("GET", urls, true);
+    req.open("GET", url, true);
+    console.log(req)
     req.responseType = "arraybuffer";
     req.onload =  (e) => {
-      var data = new Uint8Array(req.response);
-      //console.log(data);
-      var workbook = XLSX.read(data, {type: "array"});
-      //console.log(workbook);
-      var sheet_name_list = workbook.SheetNames;
-    //  console.log(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]));
-      //this.excel = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-      //return XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-      return XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]).toPromise()
-      //.pipe(tap(response));
-    };
-    req.send()
-  
-  }
-
-   set_excel(excel){
-    this.excel = excel;
-  }
-
-  /*
-  get(req){
-    
-    req.onload =  (e) => {
         var data = new Uint8Array(req.response);
-        //console.log(data);
+        console.log(data);
         var workbook = XLSX.read(data, {type: "array"});
-        //console.log(workbook);
+        console.log(workbook);
         var sheet_name_list = workbook.SheetNames;
-      //  console.log(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]));
-        //this.excel = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-        //return XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-        return XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+        console.log(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]));
+        //var  data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+        
       };
-    req.send()
+    return req.send(this.data);
+    //return this.data;
 
+    
   }
 
-*/
-
-
-
+  getjson(){
+    let url = "/assets/csv.json";
+    return this.http.get(url);
 
   }
-
+  
+  public getJSON(): Observable<any> {
+    return this.http.get("/assets/csv.json");
+}
+}
